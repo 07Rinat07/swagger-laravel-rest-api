c4dbd23a31e9d83e16ca1d4a5b1a07bf8012cde0
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-06 19:55:06 +0500

    Getting the General List_

--- app/Http/Controllers/Swagger/PostController.php
+++ app/Http/Controllers/Swagger/PostController.php
@@ -27,13 +27,32 @@ use OpenApi\Annotations as OA;
  *         description="Ok",
  *         @OA\JsonContent(
  *             @OA\Property(property="data", type="object",
- * *               @OA\Property(property="id", type="integer", example=1),
+ *                 @OA\Property(property="id", type="integer", example=1),
  *                 @OA\Property(property="title", type="string", example="Some title"),
  *                 @OA\Property(property="likes", type="integer", example=20),
  *             ),
  *         ),
  *     ),
  * ),
+ *
+ *
+ * @OA\Get(
+ *      path="/api/posts",
+ *      summary="Список",
+ *      tags={"Post"},
+ *
+ *      @OA\Response(
+ *          response="200",
+ *          description="Ok",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="data", type="array", @OA\Items(
+ *                  @OA\Property(property="id", type="integer", example=1),
+ *                   @OA\Property(property="title", type="string", example="Some title"),
+ *                   @OA\Property(property="likes", type="integer", example=20),
+ *              )),
+ *             ),
+ *         ),
+ *      ),
  */
 class PostController extends Controller
 {
