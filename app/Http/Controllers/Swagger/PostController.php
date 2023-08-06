4b8ea298a11ca7a272f232344b9f87d2d92ddc79
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-06 21:28:31 +0500

    Update

--- app/Http/Controllers/Swagger/PostController.php
+++ app/Http/Controllers/Swagger/PostController.php
@@ -11,6 +11,7 @@ use OpenApi\Annotations as OA;
  *     path="/api/posts",
  *     summary="Создание",
  *     tags={"Post"},
+ *
  *     @OA\RequestBody(
  *         @OA\JsonContent(
  *             allOf={
@@ -50,9 +51,9 @@ use OpenApi\Annotations as OA;
  *                   @OA\Property(property="title", type="string", example="Some title"),
  *                   @OA\Property(property="likes", type="integer", example=20),
  *              )),
- *             ),
  *         ),
  *      ),
+ * ),
  *
  * @OA\Get(
  *       path="/api/posts/{post}",
@@ -77,7 +78,46 @@ use OpenApi\Annotations as OA;
  *          ),
  *       ),
  * ),
+ *
+ * @OA\Patch(
+ *        path="/api/posts/{post}",
+ *        summary="Update",
+ *        tags={"Post"},
+ *        @OA\Parameter(
+ *            description="ID post",
+ *            in="path",
+ *            name="post",
+ *            required=true,
+ *            example=2
+ *        ),
+ *
+ *     @OA\RequestBody(
+ *          @OA\JsonContent(
+ *              allOf={
+ *                   @OA\Schema(
+ *                       @OA\Property(property="title", type="string", example="Some title for Edit"),
+ *                       @OA\Property(property="likes", type="integer", example=44),
+ *                   )
+ *              }
+ *          )
+ *      ),
+ *
+ *        @OA\Response(
+ *            response="200",
+ *            description="Ok",
+ *            @OA\JsonContent(
+ *               @OA\Property(property="data", type="object",
+ *                   @OA\Property(property="id", type="integer", example=2),
+ *                   @OA\Property(property="title", type="string", example="Some title"),
+ *                   @OA\Property(property="likes", type="integer", example=20),
+ *               ),
+ *           ),
+ *        ),
+ *  ),
+ *
+
  */
+
 class PostController extends Controller
 {
 
